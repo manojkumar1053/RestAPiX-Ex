90fccd8fe40fe011e93467a33364f14a830bf73a
MY <manojkumar1053@gmail.com>
2018-10-15 19:47:23 -0400

    v2

--- src/main/java/com/teamtreehouse/review/Review.java
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -1,22 +1,21 @@
 package com.teamtreehouse.review;
 
 
+import com.teamtreehouse.core.BaseEntity;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Review {
+public class Review extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private final Long id;
     private int rating;
     private String description;
 
-    protected Review(){
-         id =null;
+    protected Review() {
+        super();
     }
 
     public int getRating() {

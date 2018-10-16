ec202866272e63210ed67925b0ff972a59f2ad45
MY <manojkumar1053@gmail.com>
2018-10-15 21:05:58 -0400

    v5

--- src/main/java/com/teamtreehouse/core/DatabaseLoader.java
+++ src/main/java/com/teamtreehouse/core/DatabaseLoader.java
@@ -2,10 +2,15 @@ package com.teamtreehouse.core;
 
 import com.teamtreehouse.course.Course;
 import com.teamtreehouse.course.CourseRepository;
+import com.teamtreehouse.review.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
@@ -18,8 +23,35 @@ public class DatabaseLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Course course = new Course("Java Basics","https://teamtreehouse.com/l/java");
+        Course course = new Course("Java Basics", "https://teamtreehouse.com/l/java");
+        course.addReview(new Review(3, "You Are Awesome !!!"));
         courses.save(course);
+        String[] templates = {
+                "Up and runnig with %s",
+                "%s basics",
+                "%s for beginners",
+                "%s for neckbeards",
+                "Under the hood"
+        };
 
+        String[] buzzwords = {
+                "Spring Rest Data",
+                "Java 9",
+                "Scala",
+                "Hibernate"
+
+        };
+
+        List<Course> bunchOfCourses = new ArrayList<>();
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String template = templates[i % templates.length];
+                    String buzzword = buzzwords[i % buzzwords.length];
+                    String title = String.format(template, buzzword);
+                    Course c = new Course(title, "http://test");
+                    c.addReview(new Review(i % 5, String.format("more %s pleased!!!!", buzzword)));
+                    bunchOfCourses.add(c);
+                });
+        courses.saveAll(bunchOfCourses);
     }
 }
--- src/main/java/com/teamtreehouse/review/Review.java
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -2,20 +2,33 @@ package com.teamtreehouse.review;
 
 
 import com.teamtreehouse.core.BaseEntity;
+import com.teamtreehouse.course.Course;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Review extends BaseEntity {
 
     private int rating;
     private String description;
+    @ManyToOne
+    private Course course;
 
     protected Review() {
         super();
+    }
+
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     public int getRating() {
